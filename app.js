086187164e6181a0a9ba0d72a0e36d0325978d5f
Yani Yankov <yaniyankov@Yanis-MacBook-Air.local>
2022-09-18 15:38:04 +0300

    Creating the To Do List

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.get("/", function (req, res) {
+    var today = new Date();
+    if (today.getDay() == 6 || today.getDay() == 0) {
+        res.sendFile(__dirname + "/index.html");
+    }
+    else {
+        res.send("No");
+    }
+    
+});
+
+
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+})
