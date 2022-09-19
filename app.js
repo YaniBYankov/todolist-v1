3c0387b59383b0a39dc573400477a2c40e52e932
Yani Yankov <yaniyankov@Yanis-MacBook-Air.local>
2022-09-19 17:32:30 +0300

    add css styles

--- app.js
+++ app.js
@@ -3,27 +3,28 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items=["play"];
+let items=[];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", function (req, res) {
-    var today = new Date();
+app.use(express.static("public"));
 
-    var options = {
+app.get("/", function (req, res) {
+    let today = new Date();
+
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     }
 
-    var day = today.toLocaleDateString("en-US", options);
-    console.log(Object.keys(items).length);
+    let day = today.toLocaleDateString("en-US", options);
     res.render("list", { kindOfDay: day, newListItems: items });
 
 });
 
 app.post("/", function (req, res) {
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 });
